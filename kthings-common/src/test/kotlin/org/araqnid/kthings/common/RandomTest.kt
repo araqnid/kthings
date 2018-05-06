5034d3c1e721aaf2c602bd8d1d2ac4c1101cba34
Steve Haslam <araqnid@googlemail.com>
2018-05-06 21:29:46 +0100

    Exercise picking random element from both list and non-list

--- kthings-common/src/test/kotlin/org/araqnid/kthings/common/RandomTest.kt
+++ kthings-common/src/test/kotlin/org/araqnid/kthings/common/RandomTest.kt
@@ -2,6 +2,7 @@ package org.araqnid.kthings.common
 
 import kotlin.test.Test
 import kotlin.test.assertFails
+import kotlin.test.assertTrue
 import kotlin.test.expect
 
 class RandomTest {
@@ -12,5 +13,15 @@ class RandomTest {
 
     @Test fun pickOne_throws_exception_on_empty_collection() {
         assertFails { listOf<Any?>().pickOne() }
+    }
+
+    @Test fun pickOne_returns_element_from_list() {
+        val list = listOf("a", "b", "c", "d")
+        assertTrue(list.contains(list.pickOne()))
+    }
+
+    @Test fun pickOne_returns_element_from_set() {
+        val set = setOf("a", "b", "c", "d")
+        assertTrue(set.contains(set.pickOne()))
     }
 }
