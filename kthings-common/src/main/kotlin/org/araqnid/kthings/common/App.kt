fe528107fefd7bdd3097ea76c197d6526e6e813d
Steve Haslam <araqnid@googlemail.com>
2019-03-09 12:11:47 +0000

    Specify coroutines core library 1.1.1 and fix up compilation

--- kthings-common/src/main/kotlin/org/araqnid/kthings/common/App.kt
+++ kthings-common/src/main/kotlin/org/araqnid/kthings/common/App.kt
@@ -1,9 +1,10 @@
 package org.araqnid.kthings.common
 
-import kotlinx.coroutines.experimental.DefaultDispatcher
-import kotlinx.coroutines.experimental.Deferred
-import kotlinx.coroutines.experimental.async
-import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 
 fun app(): Deferred<Unit> {
     val arr = intArrayOf(123, 456, 789)
@@ -12,8 +13,8 @@ fun app(): Deferred<Unit> {
     println("give me a number: ${randomNumber()}")
     println("pick one! ${arr.toList().pickOne()}")
 
-    println("default coroutine dispatcher is $DefaultDispatcher (a ${DefaultDispatcher::class.simpleName})")
-    return async {
+    println("default coroutine dispatcher is ${Dispatchers.Default} (a ${Dispatchers.Default::class.simpleName})")
+    return GlobalScope.async {
         println("starting coroutine")
         delay(500)
         println("inside...")
--- kthings-jvm/src/main/kotlin/org/araqnid/kthings/JvmApp.kt
+++ kthings-jvm/src/main/kotlin/org/araqnid/kthings/JvmApp.kt
@@ -1,7 +1,7 @@
 package org.araqnid.kthings
 
-import kotlinx.coroutines.experimental.runBlocking
-import org.araqnid.kthings.common.*
+import kotlinx.coroutines.runBlocking
+import org.araqnid.kthings.common.app
 
 fun main(args: Array<String>) {
     runBlocking {
